2f0e5b7f31d231cbc2d0e2b51bf4222d824ff569
cbush19 <cbush5400@gmail.com>
2019-10-20 09:51:30 -0400

    add newpassword.html new form

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+// document.querySelector("#generatePwd").onclick = function() {
+//     alert('you clicked me');
+// }
+
+document.querySelector("#copyClip").onclick = function() {
+    alert('Your secure generated password has been copied to your clipboard');
+}
